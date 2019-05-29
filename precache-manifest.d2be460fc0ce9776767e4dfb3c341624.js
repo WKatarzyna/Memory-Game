a394184389d19c588a583948b5a070642d4a29b4
Katarzyna Wisla <wisla.katarzyna@gmail.com>
2019-05-30 00:47:30 +0200

    Updates

---
+++ precache-manifest.d2be460fc0ce9776767e4dfb3c341624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "d035c1daf013053ac749",
+    "url": "/Memory-Game/static/js/main.f36187bc.chunk.js"
+  },
+  {
+    "revision": "3fddd0e8d1645f0b8b5c",
+    "url": "/Memory-Game/static/js/2.b62a2150.chunk.js"
+  },
+  {
+    "revision": "d035c1daf013053ac749",
+    "url": "/Memory-Game/static/css/main.fa5d0271.chunk.css"
+  },
+  {
+    "revision": "8c1a55c3a954909682ead122ec977bfc",
+    "url": "/Memory-Game/index.html"
+  }
+];
