39ad544ded2a6e43471ca58a3e8548ceed6ec70c
Katarzyna Wisla <wisla.katarzyna@gmail.com>
2019-06-08 14:17:24 +0200

    updating files

--- src/App.js
+++ src/App.js
@@ -1,88 +1,81 @@
-
-import React, { useState, useEffect } from 'react'
-import Navigation from './components/navigation/Navigation';
-import Board from './components/main/board/board';
-import Setgame from './utils/layer';
-import Footer from './components/main/footer/footer'
+import React, { useState, useEffect } from "react";
+import Navigation from "./components/navigation/Navigation";
+import Board from "./components/main/board/board";
+import Setgame from "./utils/layer";
+import Footer from "./components/main/footer/footer";
 
 function App() {
-
-  let [solved, setSolved] = useState([])// śledzenie odwrócenia karty
-  let [settingCards, setCardsLayer] = useState([])// śledzenie id karty
-  let[turn, setTurn]= useState(false)//odwracanie kart do 2 
-  let [cards, setCards] = useState([])
+  let [solved, setSolved] = useState([]); // śledzenie odwrócenia karty
+  let [settingCards, setCardsLayer] = useState([]); // śledzenie id karty
+  let [turn, setTurn] = useState(false); //odwracanie kart do 2
+  let [cards, setCards] = useState([]);
   useEffect(() => {
-    setCards(Setgame())
-  }, [])
-  
-  let sameCardClickedTwice = (id) => settingCards.includes(id)
+    setCards(Setgame());
+  }, []);
 
-  let cardMatch = (id) => {
-    const clickedCard = cards.find((card) => card.id === id)
+  let sameCardClickedTwice = id => settingCards.includes(id);
+
+  let cardMatch = id => {
+    const clickedCard = cards.find(card => card.id === id);
     console.log(clickedCard);
-    const flippedCard = cards.find((card) => settingCards[0] === card.id)
+    const flippedCard = cards.find(card => settingCards[0] === card.id);
     console.log(flippedCard);
-    return flippedCard.type === clickedCard.type
-    
-  }
+    return flippedCard.type === clickedCard.type;
+  };
   const resetButton = () => {
-    setCardsLayer([])
-    setTurn(false)
-    setSolved([])
-  }
+    setCardsLayer([]);
+    setTurn(false);
+    setSolved([]);
+  };
   const resetCards = () => {
-    setCardsLayer([])
-    setTurn(false)
-  }
+    setCardsLayer([]);
+    setTurn(false);
+  };
 
-  const handleClick = (id) => {
-    setTurn(true)
+  const handleClick = id => {
+    setTurn(true);
     if (settingCards.length === 0) {
-      
-      setCardsLayer((settingCards) => [...settingCards, id])
-      setTurn(false)
+      setCardsLayer(settingCards => [...settingCards, id]);
+      setTurn(false);
     } else {
-      if (sameCardClickedTwice(settingCards, id)) return
-      setCardsLayer((setCards) => [...settingCards, id])
+      if (sameCardClickedTwice(settingCards, id)) return;
+      setCardsLayer(setCards => [...settingCards, id]);
       if (cardMatch(id)) {
-        setSolved([...solved, ...settingCards, id])//odwołanie do atrybutu obiektu cards
-        resetCards()
+        setSolved([...solved, ...settingCards, id]); //odwołanie do atrybutu obiektu cards
+        resetCards();
       } else {
-        setTimeout(resetCards, 700)
+        setTimeout(resetCards, 700);
       }
     }
-  }
+  };
 
   return (
     <div className="container">
-    <nav>
-      <Navigation/>
-      <button 
-       className="reset"
-       onClick={(
-       ) => ( resetButton(
-         setSolved([]),
-         setCardsLayer([]),
-         setTurn(false),
-        
-       ))} >Reset Game</button>
+      <nav>
+        <Navigation />
+        <button
+          className="reset"
+          onClick={() =>
+            resetButton(setSolved([]), setCardsLayer([]), setTurn(false))
+          }
+        >
+          Reset Game
+        </button>
       </nav>
       <main>
-       
         <div className="board-content">
-        <Board
-        cards={cards}
-        flipped={settingCards}
-        handleClick={handleClick}
-        solved={solved}
-        turn={turn}/>
+          <Board
+            cards={cards}
+            flipped={settingCards}
+            handleClick={handleClick}
+            solved={solved}
+            turn={turn}
+          />
         </div>
-        
-       </main>
-       <Footer />
-     </div>);
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
- 
--- src/utils/layer.js
+++ src/utils/layer.js
@@ -10,21 +10,25 @@
 //   return _array
 // }
 
- function Setgame() {
-  let id = 0
-  const cards = ['1', '5', '6', '4','3','2'
-  ].reduce((acc, type) => {
-    acc.push(...[{
-      id: id++,
-      type
-    }, {
-      id: id++,
-      type
-    }])
-    return acc
-  }, [])
+function Setgame() {
+  let id = 0;
+  const cards = ["1", "5", "6", "4", "3", "2"].reduce((accumulator, type) => {
+    accumulator.push(
+      ...[
+        {
+          id: id++,
+          type
+        },
+        {
+          id: id++,
+          type
+        }
+      ]
+    );
+    return accumulator;
+  }, []);
 
-  return(cards)
+  return cards;
 }
 
 export default Setgame;
