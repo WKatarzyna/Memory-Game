1eff61738a16407c4709bf7196111d1f1f2b3c32
WKatarzyna <wisla.katarzyna@gmail.com>
2020-02-11 23:19:23 +0100

    Update layer.js

--- src/utils/layer.js
+++ src/utils/layer.js
@@ -1,14 +1,3 @@
-// function shuffle(array) {
-//   const _array = array.slice(0)
-//   for (var i = _array.length - 1; i > 0; i--) {
-//     var j = Math.floor(Math.random() * (i + 1))
-//     var temp = _array[i]
-//     _array[i] = _array[j]
-//     _array[j] = temp
-//   }
-
-//   return _array
-// }
 
 function Setgame() {
   let id = 0;
